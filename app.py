036e0b1c19cd4c4d2d8fdd2f143d97a102da0f10
FDAAlmaviva <137777049+FDAAlmaviva@users.noreply.github.com>
2023-06-27 09:35:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    name= request.arg.get("name")
+    response = { "data" : f"Hello, {name} !" }
+    return jsonify(response)
